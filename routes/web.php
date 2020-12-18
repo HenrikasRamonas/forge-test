eb63aeed0f58d92e0ab7ac9c655aa771a9b66841
kadev0502 <kadev0502@gmail.com>
2020-12-18 15:29:51 +0100

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('forge', function () {
+    return '<h1>Working Fine!!!</h1>';
+});
+
